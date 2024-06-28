8ffd67715ccbb976bd3d9898c4ee2992a7d8acc0
chuanhuiw <142211269+chuanhuiw@users.noreply.github.com>
2024-06-28 21:58:50 +0800

    Update index.jsx

--- src/components/Signup/index.jsx
+++ src/components/Signup/index.jsx
@@ -21,7 +21,7 @@ const Signup = () => {
     const handleSubmit = async(e) => {
         e.preventDefault();
         try {
-            const url = "https://focusfish-backend.vercel.app/api/users";
+            const url = "https://focusfish-backend.onrender.com/api/users";
             const { data: res } = await axios.post(url, data);
             navigate("/login");
             console.log(res.message);
